904b3878d233c5c0253c53a11a0f3778d04d0993
datanji <154020960+datanji@users.noreply.github.com>
2025-03-19 16:57:54 +0100

    Update csv2itaca.js

--- csv2itaca.js
+++ csv2itaca.js
@@ -1,6 +1,6 @@
 /**
  * Nombre del programa: csv2itaca
- * Versión: 1.2.2
+ * Versión: 1.2.3
  * Autor: David Palazón.
  * Repositorio: https://github.com/da-bid/csv2itaca
  * 
@@ -13,15 +13,16 @@ function querySelectorByXPath(xpath) {
   return result.singleNodeValue;
 }
 
-/*Si no hay menú lo creamos*/
+/*Si no hay menú lo creamos.*/
 if (!document.getElementById("csvFileInput")){
   /*Buscamos el header*/
   element = querySelectorByXPath('//section[@id="imc-seccio-avaluacio"]/div/div[@class="imc-contingut-carregat"]');
   header = element.querySelector("header");
 
-  /*Agregar botón de cargar CSV*/
+  /* Agregar botón de cargar CSV */
   container = document.createElement("div");
   container.style="background: hsla(195, 79.9%, 48.8%);"
+  container.id="csvnotasloader"
   container.innerHTML = `<p>Sube un archivo CSV con las notas: <input type="file" id="csvFileInput" accept=".csv"></p>`;
   header.appendChild(container);
 }
@@ -37,19 +38,26 @@ document.getElementById('csvFileInput').addEventListener('change', function(even
     const data = text.split('\n').map(row => row.split(';'));
     document.getElementById('csvFileInput').value=null;
     
-    /*Capturamos los botones*/
+    /* Capturamos los botones*/
     const notasArr = document.querySelectorAll("input.imc-f-qu-camp");
     const obsArr = document.querySelectorAll("a.imc-bt-observacions");
     let final;
-    
+    let comm;
     /*Volcamos los datos */
     for (i = 0; i < data.length && i<obsArr.length; i++) {
-      /*Escribimos la nota */
-      notasArr[i].value = data[i][0];
-      obsArr[i].click()
-      document.querySelector("textarea.imc-f-observacions-avanzada").value = String(data[i][1]).replace(/\\n/g, '\n');
-      final = document.querySelectorAll("a.imc-bt-finalitza")
-      final[2].click();
+      //Escribimos la nota */
+      if (!(typeof (data[i])=="object" && data[i].length>1)){
+        notasArr[i].value = data[i];
+      } else {
+        notasArr[i].value = data[i][0];
+        obsArr[i].click()
+        comm = String(data[i][1])
+        if (comm.length && comm.length>0){
+          document.querySelector("textarea.imc-f-observacions-avanzada").value = comm.replace(/\\n/g, '\n');
+          final = document.querySelectorAll("a.imc-bt-finalitza")
+          final[2].click();
+        }
+      }
     }
     
   };
